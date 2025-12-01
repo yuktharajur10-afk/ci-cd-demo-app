1c8f125b40cf9fea1d84f69d1b0bc66aa98c37fc
yuktharajur10-afk <yuktharajur10@gmail.com>
2025-12-01 17:41:18 +0100

    Update HelloController.java

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -1,11 +1,17 @@
 package com.example;
 
 public class HelloController {
+
     public String sayHello() {
         return "Hello, CI/CD!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(new HelloController().sayHello());
+
+        // Keep the app alive indefinitely
+        while (true) {
+            Thread.sleep(1000); // sleep 1 second
+        }
     }
 }
