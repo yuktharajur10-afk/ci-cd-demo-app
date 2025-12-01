9e7d3c975affece8bf9e8d9208175cde14596b9d
yuktharajur10-afk <yuktharajur10@gmail.com>
2025-12-01 18:37:44 +0100

    Update HelloTest.java

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,13 +1,19 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloTest {
 
     @Test
-    public void testSayHello() {
-        HelloController hc = new HelloController();
-        assertEquals("Hello, CI/CD!", hc.sayHello());
+    public void testServerResponse() throws Exception {
+        URL url = new URL("http://localhost:8080/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        assertEquals(200, responseCode);
     }
 }
